663f6e3dca5539943740faf3b1fe6d9dc2f8becf
Leandro Braga <ldrbraga@gmail.com>
2020-07-28 18:47:00 -0300

    Fix multi carousel selectors

--- assets/scripts/multi-carousel.js
+++ assets/scripts/multi-carousel.js
@@ -1,11 +1,35 @@
-const scrollL = () => {
-  let x = document.getElementsByClassName("multi-carousel")[0];
+const scrollLeftCategory = () => {
+  let x = document.querySelector(".category div");
   let step = window.outerWidth / 2;
   x.scrollLeft -= step;
 }
 
-const scrollR = () => {
-  let x = document.getElementsByClassName("multi-carousel")[0];
+const scrollRightCategory = () => {
+  let x = document.querySelector(".category div");
+  let step = window.outerWidth / 2;
+  x.scrollLeft += step;
+}
+
+const scrollLeftShop = () => {
+  let x = document.querySelector(".shop-carousel__carousel div");
+  let step = window.outerWidth / 2;
+  x.scrollLeft -= step;
+}
+
+const scrollRightShop = () => {
+  let x = document.querySelector(".shop-carousel__carousel div");
+  let step = window.outerWidth / 2;
+  x.scrollLeft += step;
+}
+
+const scrollLeftInsta = () => {
+  let x = document.querySelector(".instagram__carousel div");
+  let step = window.outerWidth / 2;
+  x.scrollLeft -= step;
+}
+
+const scrollRightInsta = () => {
+  let x = document.querySelector(".instagram__carousel div");
   let step = window.outerWidth / 2;
   x.scrollLeft += step;
 }
