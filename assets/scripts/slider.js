323c186cd62c53d15cb356626a9c458e0d72d84c
Leandro Braga <ldrbraga@gmail.com>
2020-07-29 13:02:29 -0300

    Create slider

---
+++ assets/scripts/slider.js
@@ -0,0 +1,31 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+const plusSlides = (n) => {
+  showSlides(slideIndex += n);
+}
+
+const currentSlide = (n) => {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  const slides = document.getElementsByClassName("slides__item");
+  const dots = document.getElementsByClassName("dots__item");
+  let i;
+
+  if (n > slides.length) slideIndex = 1 
+
+  if (n < 1) slideIndex = slides.length 
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
