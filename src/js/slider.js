fd85a2856bbfa81e4cfb828f1e6e678138d74b02
Renan Lima <renanlimabl@gmail.com>
2021-01-26 17:23:14 -0300

    add section slider collections mobile

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,6 +1,18 @@
-// $('.slider').slick({
-//   centerMode: true,
-//   centerPadding: '60px',
-//   slidesToShow: 3,
-//   arrows: false,
-// });
+$('.slider').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '60px',
+        nextArrow: false,
+      }
+    }
+  ]
+});
