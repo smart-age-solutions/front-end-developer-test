162c528f1e13ee3605b1f5b2f62363a8ca465945
Renan Lima <renanlimabl@gmail.com>
2021-01-26 09:24:19 -0300

    add gulp build to dist

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,17 @@ gulp.task('js', async function () {
     .pipe(gulp.dest('./dist/js'))
 })
 
+gulp.task('build', async function () {
+  gulp.src('./src/js/*.js')
+    .pipe(await babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(gulp.dest('./dist/js'))
+  gulp.src('./src/scss/**/*.scss')
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'));
+})
+
 gulp.task('watch', function () {
   gulp.watch('./src/scss/**/*.scss', gulp.series('scss'));
 })
