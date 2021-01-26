d917841fc77acc05b849db83d36ffde074aefe84
Renan Lima <renanlimabl@gmail.com>
2021-01-26 14:08:40 -0300

    add section slider category mobile / desktop

---
+++ src/js/categoryRings.js
@@ -0,0 +1,20 @@
+$('.category-rings-image').slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  centerMode: true,
+  centerPadding: '300px',
+  prevArrow: false,
+  nextArrow: $('.next-arrow'),
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '40px',
+        nextArrow: false,
+      }
+    }
+  ]
+});
