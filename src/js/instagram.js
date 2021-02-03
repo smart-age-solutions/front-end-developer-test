cb60d83173d8bd2840283e9590f1badc4ad8420c
Rafael Pereira <rafaelpereira2.0@outlook.com>
2021-02-03 19:05:13 -0300

    Update instagram section

---
+++ src/js/instagram.js
@@ -0,0 +1,10 @@
+$('.instagram-carrousel').slick({
+    prevArrow: false,
+    nextArrow: false,
+    centerMode: true,
+    slidesToShow: 1
+    // infinite: true,
+    // speed: 500,
+    // autoplay: true
+    // autoplaySpeed: 3000
+  });
