763d45c5e68c273c83e27260f2ff14da483cd417
Rafael Pereira <rafaelpereira2.0@outlook.com>
2021-02-08 10:11:04 -0300

    Update hero slider

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,16 +1,31 @@
-// Hero carrousel
-$('.hero-carrousel-items').slick({
-    prevArrow: false,
-    nextArrow: false,
+//Current slide and button
+let currentSlide = document.querySelector('.carrousel-item');
+let currentNavButton = document.querySelector('.nav__item');
+
+//Navigation
+const navigation = document.querySelectorAll('.nav__item');
+
+//Foreach nav-element clicked set slide visible
+navigation.forEach((value, i) => {
+    value.addEventListener("click", function () {
+
+        setBorder(value);
+
+        const selected = document.querySelectorAll('.carrousel-item')[i];
+
+        setVisibility(selected);
+    });
 });
 
-$('.nav-items').slick({
-    prevArrow: false,
-    nextArrow: false,
-    
-    
-    slidesToShow: 3,
-    asNavFor: '.hero-carrousel-items',
-    focusOnSelect: true
-});
+function setBorder(value){
+    currentNavButton.style.border = 'none';
+    value.style.border = 'solid 1px rgba(213, 163, 104, 1)';
+    value.style.borderRadius = '100%';
+    currentNavButton = value;
+}
 
+function setVisibility(selected){
+    currentSlide.style.display = "none";
+    selected.style.display = 'flex';
+    currentSlide = selected;
+}
