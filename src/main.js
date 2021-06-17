a37e1b987e8bfb75011e2f86362180e79101663c
Renato-Alencar <renato.augustoalencar@gmail.com>
2021-06-16 23:55:32 -0300

    Importa os arquivos da biblioteca Glide.js e adiciona a função de inicialização do slider.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Glide from '@glidejs/glide'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+new Glide('.glide', {
+  type: 'slider',
+  startAt: 0,
+  perView: 1
+}).mount();
