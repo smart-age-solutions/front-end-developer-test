ab3bb7f9b239dfc4acdb0d0b0248a8d24ceb381b
Renato-Alencar <renato.augustoalencar@gmail.com>
2021-06-24 14:58:38 -0300

    Atualiza o component App.

--- src/App.vue
+++ src/App.vue
@@ -9,19 +9,30 @@
       </nav>
     </header>
     <glide />
-    <div class="parte-two"></div>
-
+    <originalDesign />
+    <jewelryStore />
+    <classicBridal />
+    <category />
   </div>
 </template>
 
 <script>
 
 import glide from '@/components/Slider'
+import originalDesign from '@/components/OriginalDesignPage'
+import jewelryStore from '@/components/JewelryStore'
+import classicBridal from '@/components/ClassicBridal'
+import category from '@/components/Category'
 
 export default {
   name: 'App',
   components: {
-    glide
+    glide,
+    originalDesign,
+    jewelryStore,
+    classicBridal,
+    category
+
   },
   data() {
     return {
@@ -46,33 +57,73 @@ $font-color-primary: #D5A368;
 $bg-seconday: #472138;
 $bg-instagram: #999595;
 $bg-contact: #333333;
-* {
-  padding: 0;
-  margin: 0;
+/* http://meyerweb.com/eric/tools/css/reset/
+   v2.0 | 20110126
+   License: none (public domain)
+*/
+
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure,
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
 }
 #app {
-  width: 100vw;
-  height: auto;
-
+  overflow: hidden;
   #header {
-    width:100%;
-    height: 11.4375rem;
-
-    border: 1px solid black;
+    width: 100vw;
+    height: 20vh;
 
     #logo {
       display: block;
 
-      width: 30%;
       height: 30%;
 
-      margin: 3rem auto 1rem auto;
+      padding-top: 7vh;
+      margin: 0 auto;
     }
     #navbar {
       width: 50%;
-      height: auto;
 
-      margin: .2rem auto 1.375rem auto;
+      margin: 0 auto;
+      padding-top: 3vh;
 
       display: flex;
       flex-direction: row;
@@ -80,16 +131,12 @@ $bg-contact: #333333;
       justify-content: space-between;
       align-items: center;
 
-      padding: 1rem 0 .7rem 0;
       li {
         list-style: none;
 
         .options {
-          font-size: .875rem;
+          font-size: 1vw;
           font-family: Raleway;
-          font-style: Medium;
-
-          letter-spacing: 10%;
 
           text-decoration: none;
 
@@ -97,13 +144,6 @@ $bg-contact: #333333;
         }
       }
     }
-  }
-  .parte-two {
-    background-color: $font-color-primary;
-
-    width: 100vw;
-    height: 100vh;
-
   }
 }
 </style>
