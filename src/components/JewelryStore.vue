b1a2fe0e6a3e9f958bb4276d67842d53698cc1e3
Renato-Alencar <renato.augustoalencar@gmail.com>
2021-06-24 14:56:19 -0300

    Cria, estiliza e adiciona o component JewelryStore.

---
+++ src/components/JewelryStore.vue
@@ -0,0 +1,64 @@
+<template>
+    <section class="jewelry-store">
+        <div class="jewelry_container">
+            <div class="container_text_up">THE LATEST FROM</div>
+            <h1 class="container_title">Jewelry Store</h1>
+            <p class="container_text">Discover all that’s new, brilliant and beautiful in the world of Brand's Name.</p>
+            <btnChevron :text="textBtnChevron" :styleIcon="styleIcon"/>
+        </div>
+    </section>
+</template>
+
+<script>
+import btnChevron from '@/components/ChevronButton'
+
+export default {
+    name: 'jewelryStore',
+    components: {
+        btnChevron,
+    },
+    data() {
+        return {
+            textBtnChevron: 'SEE MORE',
+            styleIcon: {
+                color: 'black'
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .jewelry-store {
+        width: 100vw;
+        background-color: white;
+
+        .jewelry_container {
+            height: 30vh;
+            margin: 9vh 5vw;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            align-items: center;
+
+            .container_text_up {
+                font-size: 1.2vw;
+                font-weight: 400;
+
+                letter-spacing: .3vw;
+            }
+            .container_title {
+                font-size: 5vw;
+                font-weight: 700;
+
+                letter-spacing: .5vw;
+
+                color: #D5A368;
+            }
+            .container_text {
+                font-size: 1.2vw;
+                font-weight: 700;
+            }
+        }
+    }
+</style>
