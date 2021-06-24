803f76cb6611b5f9d3926cf05d858758905b1ab4
Renato-Alencar <renato.augustoalencar@gmail.com>
2021-06-24 14:48:22 -0300

    Adiciona a estrutura dos carrosséis da biblioteca Glide.js

--- src/main.js
+++ src/main.js
@@ -11,5 +11,17 @@ new Vue({
 new Glide('.glide', {
   type: 'slider',
   startAt: 0,
-  perView: 1
+  gap:0,
+  perView: 1,
+  animationDuration: 0,
+  keyboard: false,
+}).mount();
+
+new Glide('#category', {
+  type: 'carousel',
+  startAt: 0,
+  perView: 1,
+  focusAt: 0,
+  keyboard: 'true',
+  perTouch: 1,
 }).mount();
