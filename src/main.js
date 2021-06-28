fd3753362aced0083a316ed5bf3de7ac1f40baa7
Renato-Alencar <renato.augustoalencar@gmail.com>
2021-06-28 13:47:00 -0300

    Adiciona a estrutura JS dos carrosséis e importa o arquivo de Reset.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Glide from '@glidejs/glide'
+// import './assets/reset/reset.css'
 
 Vue.config.productionTip = false
 
@@ -11,10 +12,11 @@ new Vue({
 new Glide('.glide', {
   type: 'slider',
   startAt: 0,
-  gap:0,
+  gap: 0,
   perView: 1,
   animationDuration: 0,
   keyboard: false,
+  perTouch: 0,
 }).mount();
 
 new Glide('#category', {
@@ -24,4 +26,60 @@ new Glide('#category', {
   focusAt: 0,
   keyboard: 'true',
   perTouch: 1,
+}).mount();
+
+new Glide('#carousel-shop', {
+  type: 'carousel',
+  startAt: 2,
+  perView: 4,
+  focusAt: 'center',
+  keyboard: 'true',
+  perTouch: 1,
+  rewind: true,
+  breakpoints: {
+    1023: {
+      perView: 3,
+    },
+    767: {
+      perView: 2,
+    }
+  }
+}).mount();
+
+new Glide('#collection__carousel', {
+  type: 'slider',
+  startAt: 0,
+  gap: 0,
+  perView: 1,
+  animationDuration: 0,
+  keyboard: false,
+  perTouch: 0,
+}).mount();
+
+
+new Glide('#collection__carousel', {
+  type: 'slider',
+  startAt: 0,
+  gap: 0,
+  perView: 1,
+  animationDuration: 0,
+  keyboard: false,
+  perTouch: 0,
+}).mount();
+
+new Glide('#about-us__carousel', {
+  type: 'slider',
+  startAt: 'center',
+  perView: 3,
+  animationDuration: 0,
+  keyboard: false,
+}).mount();
+
+new Glide('#instagram__carousel', {
+  type: 'carousel',
+  startAt: 3,
+  focusAt: 'center',
+  perView: 6,
+  gap: 10,
+  keyboard: true,
 }).mount();
