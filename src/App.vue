537959ab05c93db22fcfa0cad1a33e59173da1ab
Renato-Alencar <renato.augustoalencar@gmail.com>
2021-06-28 13:49:37 -0300

    Remove o reset.css e adiciona o estrutura dos vetores no Header.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
   <div id="app">
     <header id="header">
+      <div class="header__vectors">
+        <button class="vectors__location">
+          <div class="location__icon"></div>
+          <div class="location__text">Find a Store</div>
+        </button>
+        <div class="vectors__icons">
+          <div class="icons__user"></div>
+          <div class="icons__hearth"></div>
+          <div class="icons__bag"></div>
+          <div class="icons__search"></div>
+        </div>
+      </div>
       <img id="logo" src="../src/assets/smart-age-logo.svg" alt="logotipo do Smart Age Solutions">
       <nav id="navbar">
         <li v-for="option in navOptions" :key="option.id">
@@ -8,11 +20,18 @@
         </li>
       </nav>
     </header>
+
     <glide />
     <originalDesign />
     <jewelryStore />
     <classicBridal />
     <category />
+    <shop/>
+    <collection />
+    <aboutUs />
+    <events />
+    <instagram />
+    <stores />
   </div>
 </template>
 
@@ -23,6 +42,12 @@ import originalDesign from '@/components/OriginalDesignPage'
 import jewelryStore from '@/components/JewelryStore'
 import classicBridal from '@/components/ClassicBridal'
 import category from '@/components/Category'
+import shop from '@/components/Shop'
+import collection from '@/components/Collection'
+import aboutUs from '@/components/AboutUs'
+import events from '@/components/Events'
+import instagram from '@/components/Instagram'
+import stores from '@/components/Stores'
 
 export default {
   name: 'App',
@@ -31,8 +56,13 @@ export default {
     originalDesign,
     jewelryStore,
     classicBridal,
-    category
-
+    category,
+    shop,
+    collection,
+    aboutUs,
+    events,
+    instagram,
+    stores,
   },
   data() {
     return {
@@ -48,7 +78,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 
 $bg-general: #FFFFFF;
 $font-color-general: #000000;
@@ -57,73 +87,63 @@ $font-color-primary: #D5A368;
 $bg-seconday: #472138;
 $bg-instagram: #999595;
 $bg-contact: #333333;
-/* http://meyerweb.com/eric/tools/css/reset/
-   v2.0 | 20110126
-   License: none (public domain)
-*/
 
-html, body, div, span, applet, object, iframe,
-h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-a, abbr, acronym, address, big, cite, code,
-del, dfn, em, img, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-b, u, i, center,
-dl, dt, dd, ol, ul, li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed,
-figure, figcaption, footer, header, hgroup,
-menu, nav, output, ruby, section, summary,
-time, mark, audio, video {
-	margin: 0;
-	padding: 0;
-	border: 0;
-	font-size: 100%;
-	font: inherit;
-	vertical-align: baseline;
-}
-/* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure,
-footer, header, hgroup, menu, nav, section {
-	display: block;
-}
-body {
-	line-height: 1;
-}
-ol, ul {
-	list-style: none;
-}
-blockquote, q {
-	quotes: none;
-}
-blockquote:before, blockquote:after,
-q:before, q:after {
-	content: '';
-	content: none;
-}
-table {
-	border-collapse: collapse;
-	border-spacing: 0;
-}
 #app {
   overflow: hidden;
   #header {
     width: 100vw;
     height: 20vh;
 
+    display: flex;
+    flex-direction: column;
+
+    justify-content: space-around;
+    align-items: center;
+
+    .header__vectors {
+      width: 100%;
+
+      display: inline-flex;
+
+      justify-content: space-between;
+      align-items: center;
+
+      .vectors__location {
+        .location__icon {
+          background-image: url('../src/assets/location-vector.png');
+        }
+        .location__text {
+          font-size: 1.2vw;
+        }
+      }
+      .vectors__icons {
+        .icons__users {
+          background-image: url('../src/assets/user-vector.png');
+        }
+        .icons__hearth {
+          background-image: url('../src/assets/hearth-vector.png');
+        }
+        .icons__bag {
+          background-image: url('../src/assets/bag-vector.png');
+        }
+        .icons__search {
+          background-image: url('../src/assets/search-vector.png');
+        }
+      }
+    }
+
     #logo {
       display: block;
 
-      height: 30%;
+      width: 20%;
 
-      padding-top: 7vh;
       margin: 0 auto;
     }
     #navbar {
-      width: 50%;
+      width: 45%;
 
       margin: 0 auto;
-      padding-top: 3vh;
+      padding: 2.5vh 0;
 
       display: flex;
       flex-direction: row;
@@ -135,7 +155,7 @@ table {
         list-style: none;
 
         .options {
-          font-size: 1vw;
+          font-size: .8vw;
           font-family: Raleway;
 
           text-decoration: none;
