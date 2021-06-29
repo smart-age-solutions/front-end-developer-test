6077906763b6e5e4f74b038ac714a663d8c03371
Renato-Alencar <renato.augustoalencar@gmail.com>
2021-06-29 17:52:13 -0300

    Atualiza o posicionamento e estilização do component.

--- src/components/ClassicBridal.vue
+++ src/components/ClassicBridal.vue
@@ -1,13 +1,12 @@
 <template>
-    <section class="classic-bridal">
-        <div class="classic_container">
-            <div class="container_card">
-                <h1 class="card_title">Classic Bridal</h1>
-                <p class="card_text">
-                    Our extraordinary diamonds are chosen to celebrate a love, a hope and a passion. Brilliantly capturing our passion for diamonds, a member of the family individually hand-selects each stone within our bridal collection, so you can fall in love with a diamond for eternity.
-                </p>
-                <btnChevron :text="textBtnChevron" :styleIcon="styleIcon"/>
-            </div>
+    <section id="classic-bridal">
+        <div class="bridal__image"></div>
+        <div class="bridal__card">
+            <h1 class="card__title">Classic Bridal</h1>
+            <p class="card__text">
+                Our extraordinary diamonds are chosen to celebrate a love, a hope and a passion. Brilliantly capturing our passion for diamonds, a member of the family individually hand-selects each stone within our bridal collection, so you can fall in love with a diamond for eternity.
+            </p>
+            <btnChevron class="card__btn" :text="textBtnChevron" :styleIcon="styleIcon"/>
         </div>
     </section>
 </template>
@@ -32,46 +31,69 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    .classic-bridal {
-        max-width: 100vw;
+
+$marginLeft: 20%;
+$marginBot: 2vh;
+$widthDiv: 50%;
+
+    #classic-bridal {
+        width: 100vw;
         height: 100vh;
+
+        display: inline-flex;
+
         background-color: #472138;
-        color: white;
-        background-image: url(../assets/model.png);
-        background-repeat: no-repeat;
-        background-size: cover;
-        background-position: -30vw 0;
 
-        .classic_container {
-            background-color: #472138;
-            position: relative;
-            left: 53vw;
-            max-width: 50%;
-            height: 100vh;
+        .bridal__image {
+            width: $widthDiv;
+            height: 100%;
 
-            .container_card {
-                position: absolute;
+            background-image: url('../assets/model.png');
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position-x: 81%;
+        }
+        .bridal__card {
+            width: 100 - $widthDiv;
+            height: 100%;
 
-                margin-top: 25%;
-                margin-left: 25%;
+            display: flex;
+            flex-direction: column;
 
-                .card_title {
-                    max-width: 20vw;
-                    font-weight: 700;
-                    font-size: 4vw;
+            justify-content: center;
+            align-items: flex-start;
 
-                    color: #D5A368;
+            .card__title {
+                width: 50%;
 
-                    margin-bottom: 2vh;
-                }
-                .card_text {
-                    max-width: 26vw;
+                font-weight: 700;
+                font-size: 6vw;
 
-                    font-size: clamp(1.2vw, 1vw, 2vw);
+                letter-spacing: .3vw;
 
-                    margin-bottom: 4vh;
-                }
+                margin-bottom: $marginBot;
+                margin-left: $marginLeft;
 
+                color: #D5A368;
+            }
+            .card__text {
+                width: 70%;
+
+                font-size: 1.25vw;
+                font-family: system-ui;
+
+                font-weight: lighter;
+                word-spacing: .2vw;
+                letter-spacing: .1vw;
+                line-height: 1.75vw;
+
+                margin-bottom: $marginBot + 2;
+                margin-left: $marginLeft;
+
+                color: white;
+            }
+            .card__btn {
+                margin-left: $marginLeft;
             }
         }
     }
